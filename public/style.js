a6ffb6220f6cd819a6d751046f24c84c1783d71f
sonnxnewtel <124778467+sonnxnewtel@users.noreply.github.com>
2023-03-24 17:41:51 +0700

    chat room

--- public/style.js
+++ public/style.js
@@ -1,49 +1,20 @@
 var socket = io("http://localhost:3000")
 $(document).ready(function(){
-    $("#loginForm").show()
-    $("#chatForm").hide()
-    $("#btnRegister").click(function(){
-        socket.emit("client-send-username", $("#txtUsername").val())
+    $("#btnCreateRoom").click(() => {
+        socket.emit("client-create-room", $("#textCreateRoom").val())
     })
 
-    socket.on("server-send-register-err", () => {
-        alert("Tên người dùng đã được sử dụng.")
-    })
-
-    socket.on("server-send-register-success", (data) => {
-        $("#loginForm").hide()
-        $("#chatForm").show()
-        $("#userName").html(data)
-    })
-
-    socket.on("server-send-list-user-online", (listUser) => {
-        var domListUser = $("#boxContent")
-        domListUser.html("")
-        
-        listUser.forEach(userName => {
-            domListUser.append("<div class='userOnline'>"+userName+"</div>")
+    socket.on("server-send-list-room", (listRoom) => {
+        console.log(listRoom)
+        var wrapperListRoom = $("#wrapperListRoom")
+        wrapperListRoom.html("")
+        listRoom.forEach(room => {
+            wrapperListRoom.append("<div>"+room+"</div>")
         });
     })
 
-    socket.on("serve-send-logout", ()=>{
-        alert("Logout thanh cong")
-        $("#loginForm").show()
-        $("#chatForm").hide()
-    })
-
-
-    $("#btnLogout").click(() => {
-        socket.emit("client-send-logout")
-    })
-
-    $("#btnSendMessage").click(() => {
-        socket.emit("client-send-message", $("#txtMessage").val())
-        $("#txtMessage").val("")
-    })
-
-    var listMessage = $("#listMessages")
-    socket.on("server-send-message", (data) => {
-        listMessage.append("<div>"+data.userName+": "+data.message+"</div>")
+    socket.on("server-send-room-curren", (name) => {
+        $("#roomCurrent").html(name)
     })
 
 });
--- server.js
+++ server.js
@@ -10,27 +10,11 @@ server.listen(3000);
 var listUsers = []
 io.on("connection", (socket) => {
     console.log("Có người kết nối: " + socket.id);
-    socket.on("client-send-username", (userName) => {
-        console.log("client-send-username: ", userName);
-        if(listUsers.includes(userName)) {
-            socket.emit("server-send-register-err")
-        }else {
-            listUsers.push(userName)
-            socket.userName = userName
-            socket.emit("server-send-register-success", userName)
-            io.sockets.emit("server-send-list-user-online", listUsers)
-        }
-    })
-
-
-    socket.on("client-send-logout", () => {
-        listUsers.splice(listUsers.indexOf(socket.userName), 1)
-        socket.emit("serve-send-logout")
-        io.sockets.emit("server-send-list-user-online", listUsers)
-    })
-
-    socket.on("client-send-message", (message) => {
-        io.sockets.emit("server-send-message", {userName: socket.userName, message: message})
+    socket.on("client-create-room", (nameRoom) => {
+        socket.join(nameRoom)
+        socket.room = nameRoom
+        io.sockets.emit("server-send-list-room", Array.from(io.sockets.adapter.rooms.keys()))
+        socket.emit("server-send-room-curren", nameRoom)
     })
 })
 
